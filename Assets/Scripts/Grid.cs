9f90fdbed2979b46646bc48c3b857f8c065a2c2b
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-20 12:56:12 -0400

    Render Grid Vertices Gizmos.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,14 +9,42 @@ public class Grid : MonoBehaviour
 
     private Vector3[] vertices;
 
+    private Mesh mesh;
+
     private void Awake()
     {
-        Generate();
+        StartCoroutine(Generate());
     }
 
-    private void Generate()
+    private IEnumerator Generate()
     {
-        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
 
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedura; Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
     }
 }
