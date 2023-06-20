00b4edc036e140db894cb8146e93e8f0b15a6b0a
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-20 14:22:55 -0400

    Generate vertices and trangles for the procedural grid mesh

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -21,7 +21,7 @@ public class Grid : MonoBehaviour
         WaitForSeconds wait = new WaitForSeconds(0.05f);
 
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
-        mesh.name = "Procedura; Grid";
+        mesh.name = "Procedural Grid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         for (int i = 0, y = 0; y <= ySize; y++)
@@ -29,6 +29,31 @@ public class Grid : MonoBehaviour
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+        mesh.vertices = vertices;
+        /*
+        int[] triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+        triangles[3] = 1;
+        triangles[4] = xSize + 1;
+        triangles[5] = xSize + 2;*/
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                int ti = x * 6 + (y * xSize * 6);
+                int vi = x + (y * (xSize + 1));
+                triangles[ti] = vi;
+                triangles[ti + 1] = triangles[ti + 4] = xSize + vi + 1;
+                triangles[ti + 2] = triangles[ti + 3] = vi + 1;
+                triangles[ti + 5] = xSize + vi + 2;
+                mesh.triangles = triangles;
                 yield return wait;
             }
         }
